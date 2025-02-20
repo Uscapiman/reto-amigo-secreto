f7c54bcf14c55d509f18afe5a79e57e5e6a8e0bc
DamianUscapi <dubcitouscapi@gmail.com>
2025-02-20 12:42:16 -0500

    Subida de proyecto amigo secreto

---
+++ README.md
@@ -0,0 +1,18 @@
+# AMIGO SECRETO 🎁
+
+## Descripcion 📌
+Este es un proyecto interactivo donde los usuarios pueden ingresar nombres de amigos y realizar un sorteo para asignar un "Amigo Secreto" de manera aleatoria. Es una excelente práctica para mejorar habilidades en lógica de programación y manipulación del DOM con JavaScript.
+
+## Tecnologías Utilizados 🛠️
+- **HTML** -> Estructura de la aplicación.
+- **CSS** -> Estilos para mejorar la apariencia.
+- **JavaScript** -> Lógica de programación y manipulación del DOM.
+
+## Cómo Usarlo 🚀
+1. Ingresa el nombre de un amigo en el campo de texto.
+2. Haz clic en el botón "Añadir" para agregarlo a la lista.
+3. Cuando hayas agregado todos los nombres, presiona "Sortear amigo".
+4. Se mostrará el nombre de la persona seleccionada al azar.
+
+## Captura de Pantalla 🖼️
+<img src="/assets/banner.png"/>
---
+++ app.js
@@ -0,0 +1,31 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let arrayAmigo = [];
+
+function agregarAmigo() {
+    let textAmigo = document.getElementById('amigo');
+
+    if (textAmigo.value.trim() == "") {
+        alert("El cuadro de texto está vacío. Por favor, ingrese un nombre valido.");
+    } else {
+        arrayAmigo.push(textAmigo.value);
+        textAmigo.value = "";
+        listarAmigos();
+    }
+}
+
+function listarAmigos() {
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = "";
+    arrayAmigo.forEach((amigo) => {
+        let itemLista = document.createElement("li");
+        itemLista.textContent = amigo;
+        listaAmigos.appendChild(itemLista);
+    });
+}
+
+function sortearAmigo() {
+    let indiceamigo = Math.floor(Math.random() * arrayAmigo.length);
+    let amigosecreto = document.getElementById('resultado');
+    amigosecreto.innerHTML = `El amigo secreto es: ${arrayAmigo[indiceamigo]}`;
+}
+
